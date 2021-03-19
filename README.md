80b6e768770f713a3e9647d3b9350ce58cca4fa6
LucasKoval <lucaskoval@hotmail.com>
2021-03-19 19:58:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<p align="center">
+  <a href="https://www.linkedin.com/in/lucaskoval/">
+    <img alt="Avatar" src="src\images\lucas.png"/>
+  </a>
+</p>
+
+<h1 align="center">
+  Portfolio | Lucas Koval - Full Stack Developer
+</h1>
+
+## 🌟 Credits to Jacobo Martinez for the incredible work with the template
+
+- **Jacobo Martinez** - [https://github.com/cobidev](https://github.com/cobidev)
+
+## Technologies used 🛠️
+
+- [Gatsby](https://www.gatsbyjs.org/) - Static Site Generator
+- [GraphQL](https://graphql.org/) - Query language for APIs
+- [React](https://es.reactjs.org/) - Front-End JavaScript library
+- [Bootstrap 4](https://getbootstrap.com/docs/4.3/getting-started/introduction/) - Front-End UI library
+- [Sass](https://sass-lang.com/documentation) - CSS extension language
+
+## License 📄
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments 🎁
+
+I was motivated to create this project because I wanted to contribute on something useful for the dev community, thanks to [ZTM Community](https://github.com/zero-to-mastery) and [Andrei](https://github.com/aneagoie)
---
+++ src/mock/data.js
@@ -0,0 +1,90 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: 'Lucas Koval | Portfolio', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website.', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: 'Hi, my name is',
+  name: 'Lucas Koval',
+  subtitle: "and I'm a Full Stack Developer.",
+  cta: 'Know more',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'lucas.png',
+  paragraphOne: 'I was born in Corrientes, Argentina on October 20, 1988 and I currently reside in the city of La Plata, Buenos Aires. I am a Freelance Full Stack Developer oriented to e-commerce development with NodeJS (back-end) and ReactJS (front-end) as the main pillars of my work.',
+  paragraphTwo: 'I love what I do and I consider myself an organized, methodical, and collaborative person working in groups. I always seek to learn from my colleagues, as well as generate feedback that can help the team grow. My goal is to belong to a company that helps me strengthen my skills and acquire new ones.',
+  paragraphThree: '* SKILLS: JavaScript - HTML - CSS - ReactJS - NodeJS - ExpressJS - MVC Architecture - MySQL / Sequelize - APIs - Postman - Git / GitHub - Scrum - Responsive Design - Spanish (Native) - English (Intermediate).',
+  resume: 'https://drive.google.com/file/d/1JjKz8rMgnQcy5Xg71M94vVD729t6pG1J/view?usp=sharing', //https://drive.google.com/u/0/uc?id=1JjKz8rMgnQcy5Xg71M94vVD729t6pG1J&export=download
+  //if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'bbk.png',
+    title: 'Babieka - E-Commerce',
+    info: "E-Commerce website dedicated to the sale of women's footwear.",
+    info2: '* APPLIED TECHNOLOGIES: NodeJS - ExpressJS - HTML - CSS - Javascript - EJS Template Engine - APIs - MySQL / Sequelize - Git - Scrum.',
+    url: 'https://babieka.herokuapp.com/',
+    repo: 'https://github.com/LucasKoval/jfl-babieka', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'dash.png',
+    title: 'Babieka - Dashboard',
+    info: "Statistics dashboard for Babieka website.",
+    info2: '* APPLIED TECHNOLOGIES: ReactJS - APIs - Bootstrap - Git - Scrum.',
+    url: 'https://babieka-dashboard.vercel.app/',
+    repo: 'https://github.com/LucasKoval/jfl-dashboard', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'hnc.png',
+    title: 'HackerNews-clone',
+    info: "Clone of Hacker News, a social news website that focuses on computing and entrepreneurship.",
+    info2: '* APPLIED TECHNOLOGIES: ReactJS - APIs - Bootstrap - Git.',
+    url: 'https://hacker-news-clone-ten.vercel.app/',
+    repo: 'https://github.com/LucasKoval/HackerNews-clone', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: 'lucasekoval@gmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/lucaskoval/',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'https://github.com/LucasKoval',
+    },
+    {
+      id: nanoid(),
+      name: 'codepen',
+      url: 'https://codepen.io/LucasKoval',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
+};
