3e152b31fd893c06cae9aa59a8f32b284a1fb489
LucasKoval <lucaskoval@hotmail.com>
2021-04-09 11:45:04 -0300

    Details changed

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,9 +18,9 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'lucas.png',
-  paragraphOne: 'I was born in Corrientes, Argentina on October 20, 1988 and I currently reside in the city of La Plata, Buenos Aires. I am a Freelance Full Stack Developer oriented to e-commerce development with NodeJS (back-end) and ReactJS (front-end) as the main pillars of my work.',
+  paragraphOne: 'I was born and raised in Corrientes, Argentina on October 20, 1988 and I currently reside in the city of La Plata, Buenos Aires. I am a Freelance Full Stack Developer oriented to e-commerce development with NodeJS (back-end) and ReactJS (front-end) as the main pillars of my work.',
   paragraphTwo: 'I love what I do and I consider myself an organized, methodical, and collaborative person working in groups. I always seek to learn from my colleagues, as well as generate feedback that can help the team grow. My goal is to belong to a company that helps me strengthen my skills and acquire new ones.',
-  paragraphThree: 'SKILLS: JavaScript - HTML - CSS - ReactJS - NodeJS - ExpressJS - MVC Architecture - MySQL / Sequelize - APIs - Postman - Git / GitHub - Scrum - Responsive Design - Spanish (Native) - English (Intermediate).',
+  paragraphThree: 'SKILLS: JavaScript (ES6+) - HTML - CSS - ReactJS - NextJS - NodeJS - ExpressJS - MVC Architecture - MySQL / Sequelize - APIs - Postman - Git / GitHub - Scrum - Responsive Design - Spanish (Native) - English (Intermediate).',
   resume: 'https://drive.google.com/file/d/1k1b9YPU0BLAt_yJp-und3OK1Y6dQdw2X/view?usp=sharing', //https://drive.google.com/u/0/uc?id=1JjKz8rMgnQcy5Xg71M94vVD729t6pG1J&export=download
   //if no resume, the button will not show up
 };
@@ -32,7 +32,7 @@ export const projectsData = [
     img: 'babieka.png',
     title: 'Babieka - E-Commerce',
     info: "E-Commerce website dedicated to the sale of women's footwear.",
-    info2: '* Applied Technologies: NodeJS - ExpressJS - HTML - CSS - JavaScript - EJS Template Engine - APIs - MySQL / Sequelize - Git - Scrum.',
+    info2: '* Applied Technologies: NodeJS - ExpressJS - HTML - CSS - JavaScript (ES6+) - EJS Template Engine - APIs - MySQL / Sequelize - Git - Scrum.',
     url: 'https://babieka.herokuapp.com/',
     repo: 'https://github.com/LucasKoval/jfl-babieka', // if no repo, the button will not show up
   },
@@ -41,7 +41,7 @@ export const projectsData = [
     img: 'dashboard.png',
     title: 'Babieka - Dashboard',
     info: "Statistics dashboard for Babieka website.",
-    info2: '* Applied Technologies: ReactJS - APIs - Bootstrap - Git - Scrum.',
+    info2: '* Applied Technologies: ReactJS - JavaScript (ES6+) - APIs - Bootstrap - Git - Scrum.',
     url: 'https://babieka-dashboard.vercel.app/',
     repo: 'https://github.com/LucasKoval/jfl-dashboard', // if no repo, the button will not show up
   },
@@ -50,16 +50,16 @@ export const projectsData = [
     img: 'aerolab.png',
     title: "Aerolab's Catalog View",
     info: "Project made for the Aerolab coding challenge. It consists of a responsive catalog view for a loyalty program application. The main goal of the product is to help users redeem items through a points-based system.",
-    info2: '* Applied Technologies: NodeJS - ExpressJS - HTML - CSS - Bootstrap - JavaScript - EJS Template Engine - APIs - JWT - Git.',
+    info2: '* Applied Technologies: NodeJS - ExpressJS - HTML - CSS - Bootstrap - JavaScript (ES6+) - EJS Template Engine - APIs - JWT - Git.',
     url: 'https://aerolab-challenge-lkdev.herokuapp.com/',
     repo: 'https://github.com/LucasKoval/Aerolab-Challenge', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'pokedex.png',
-    title: "Pokedex Challenge",
+    title: "Pokedex Encyclopedia",
     info: "Pokemon encyclopedia where you can search and view the different pokemons and their characteristics and statistics (In progress).",
-    info2: '* Applied Technologies: NextJS - ReactJS - HTML - CSS - Bootstrap - JavaScript - APIs - Git.',
+    info2: '* Applied Technologies: ReactJS - NextJS - JavaScript (ES6+) - APIs - CSS - Bootstrap - Git.',
     url: 'https://pokedex-challenge-lkdev.vercel.app/',
     repo: 'https://github.com/LucasKoval/Pokedex-Challenge', // if no repo, the button will not show up
   },
@@ -68,7 +68,7 @@ export const projectsData = [
     img: 'hacker.png',
     title: 'Hacker News Clone',
     info: "Clone of Hacker News, a social news website that focuses on computing and entrepreneurship.",
-    info2: '* Applied Technologies: ReactJS - APIs - Bootstrap - Git.',
+    info2: '* Applied Technologies: ReactJS - JavaScript (ES6+) - APIs - Bootstrap - Git.',
     url: 'https://hacker-news-clone-lkdev.vercel.app/',
     repo: 'https://github.com/LucasKoval/HackerNews-clone', // if no repo, the button will not show up
   },
